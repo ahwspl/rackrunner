6ec333a06ac58674b97914af89285adb2c40dbd5
kedar.pagdhare <kedar.pagdhare@pharmeasy.in>
2017-09-02 20:15:26 +0530

    api

--- rackrunner/co_ordinate.py
+++ rackrunner/co_ordinate.py
@@ -1,9 +1,15 @@
 from theasims.algos import dsa
 from theasims.maps.thea2 import thea2
 
-
 def get_coordinate():
     flatmap_of_list = (dsa.flatmap(thea2.get_neighbors, thea2.locations))
     flatmap_of_list = [dict(zip(('x', 'y'), (i.x, i.y))) for i in flatmap_of_list]
-    map_location = thea2.locations
+    map_location = [i.label for i in thea2.locations]
     return dict(zip(map_location, flatmap_of_list))
+
+
+def get_boundary():
+    return {
+        "length": thea2.length,
+        "width": thea2.width
+    }
--- rackrunner/views.py
+++ rackrunner/views.py
@@ -1,13 +1,19 @@
+import json
+
 # Django imports
 from rest_framework import viewsets
 from rest_framework.response import Response
+from django.http import JsonResponse
 # Application imports
-
-
+from rackrunner.co_ordinate import get_coordinate, get_boundary
 class RackerView(viewsets.ViewSet):
-
     authentication_classes = ()
 
     def getWarehousePath(self, request):
         rack_ids = request.GET.get('racks',[])
-        return Response({'msg':'Request successfully queued'})
+        all_racks = get_coordinate()
+        boundary = get_boundary()
+        return JsonResponse({
+            "boundary" : boundary,
+            "racks" : all_racks
+        })
