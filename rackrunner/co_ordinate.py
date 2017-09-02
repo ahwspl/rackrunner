8322dd6c1a1c5712e4d8695adf29fc5f74faf80b
Hari <hariprasad@pharmeasy.in>
2017-09-02 22:42:05 +0530

    path found

--- rackrunner/co_ordinate.py
+++ rackrunner/co_ordinate.py
@@ -1,6 +1,10 @@
 from theasims.algos import dsa
 from theasims.maps.thea2 import thea2
 
+#getpath import
+from theasims.maps.structures import Location
+from theasims.algos.traverse import greedy
+
 def get_coordinate():
     flatmap_of_list = (dsa.flatmap(thea2.get_neighbors, thea2.locations))
     flatmap_of_list = [dict(zip(('x', 'y'), (i.x, i.y))) for i in flatmap_of_list]
@@ -13,3 +17,27 @@ def get_boundary():
         "length": thea2.length,
         "width": thea2.width
     }
+
+def get_path(racks):
+    if racks is None or len(racks) == 0:
+        return []
+    print(type(racks))
+    print(racks)
+    flatmap_of_list = (dsa.flatmap(thea2.get_neighbors, thea2.locations))
+    flatmap_of_list = [dict(zip(('x', 'y'), (i.x, i.y))) for i in flatmap_of_list]
+    map_location = [i.label for i in thea2.locations]
+    label_to_location_mapping = dict(zip(map_location, flatmap_of_list))
+
+    orderobj = list(map(Location, racks))
+    a = greedy(thea2, orderobj)
+
+    result = []
+    for point in a[0]:
+        if type(point) is Location:
+            temp = label_to_location_mapping[point.label]
+            temp['isPickUp'] = True
+            result.append(temp)
+        else:
+            result.append({"x": point.x, "y": point.y, "isPickUp": False})
+
+    return result
--- rackrunner/views.py
+++ rackrunner/views.py
@@ -1,19 +1,21 @@
-import json
-
 # Django imports
 from rest_framework import viewsets
-from rest_framework.response import Response
 from django.http import JsonResponse
 # Application imports
-from rackrunner.co_ordinate import get_coordinate, get_boundary
+from rackrunner.co_ordinate import get_coordinate, get_boundary, get_path
+
+
 class RackerView(viewsets.ViewSet):
     authentication_classes = ()
 
     def getWarehousePath(self, request):
-        rack_ids = request.GET.get('racks',[])
+        rack_ids = request.GET.getlist('racks')
         all_racks = get_coordinate()
         boundary = get_boundary()
+        path = get_path(rack_ids)
+
         return JsonResponse({
             "boundary" : boundary,
-            "racks" : all_racks
+            "racks" : all_racks,
+            "path" : path
         })
